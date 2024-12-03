50dce097e8bc4028d3982adac2a1080082ca4b36
SriramSudhir01 <swaglixads@gmail.com>
2024-12-03 18:26:37 +0530

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,41 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: { 
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.githubusercontent.com',
+      }
+    ]
+  },
+  webpack: (config) => {
+    config.resolve.fallback = { 
+      ...config.resolve.fallback,
+      "tunnel-agent": false 
+    };
+    return config;
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on'
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block'
+          }
+        ],
+      },
+    ];
+  }
+};
+
+module.exports = nextConfig;
